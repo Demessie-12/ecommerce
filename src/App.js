393ae002c04d6dc5e5bcdd98574e326145dd0639
demessietry <104842551+demessietry@users.noreply.github.com>
2023-11-04 16:03:24 +0300

    day-2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Shop } from './Pages/shop';
+import { ShopCatagory } from './Pages/ShopCatagory';
+import { Product } from './Pages/Product';
+import { Cart } from './Pages/Cart';
+import { LoginSignup } from './Pages/LoginSignup';
 
+// import { ShopCatagory } from './Pages/ShopCatagory';
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/mens' element={<ShopCatagory catagory="mens"/>}/>
+          <Route path='/womens' element={<ShopCatagory catagory="womens"/>}/>
+          <Route path='/Kids' element={<ShopCatagory catagory="Kid"/>}/>
+          <Route path='/product' element={<Product/>}>
+            <Route path=':productId' element={<Product/>}/>
+          </Route>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<LoginSignup/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

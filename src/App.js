1534709d8ed79d410d44997fa09e1dfded079d9e
demessietry <104842551+demessietry@users.noreply.github.com>
2023-11-07 20:39:15 +0300

    day 3

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import { Product } from './Pages/Product';
 import { Cart } from './Pages/Cart';
 import { LoginSignup } from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
+import men_banner from "./Components/Assets/banner_mens.png"
+import women_banner from "./Components/Assets/banner_women.png"
+import kid_banner from "./Components/Assets/banner_kids.png"
 
 // import { ShopCatagory } from './Pages/ShopCatagory';
 
@@ -17,9 +20,9 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path='/' element={<Shop/>}/>
-          <Route path='/mens' element={<ShopCatagory catagory="mens"/>}/>
-          <Route path='/womens' element={<ShopCatagory catagory="womens"/>}/>
-          <Route path='/Kids' element={<ShopCatagory catagory="Kid"/>}/>
+          <Route path='/mens' element={<ShopCatagory banner={men_banner} category="men"/>}/>
+          <Route path='/womens' element={<ShopCatagory banner={women_banner} category="women"/>}/>
+          <Route path='/Kids' element={<ShopCatagory banner={kid_banner} category="kid"/>}/>
           <Route path='/product' element={<Product/>}>
             <Route path=':productId' element={<Product/>}/>
           </Route>

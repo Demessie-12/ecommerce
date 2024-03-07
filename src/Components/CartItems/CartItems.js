42e1f34089b2736d8b792c07c533b0bfa28d6c35
demessietry <demessiewondimu12@gmail.com>
2024-03-07 06:04:52 +0300

    edited cart

--- src/Components/CartItems/CartItems.js
+++ src/Components/CartItems/CartItems.js
@@ -1,69 +1,109 @@
-import React, { useContext } from 'react'
-import "./CartItems.css"
-import { ShopContext } from '../../Context/ShopContext'
-import remove_icon from "../Assets/cart_cross_icon.png"
+import React, { useContext, useState } from "react";
+import "./CartItems.css";
+import { ShopContext } from "../../Context/ShopContext";
+import { showAlert } from "../Alert/Alert";
 
-const CartItems = ()=> {
-    const {all_product, cartItems, removeFromCart, getTotalCartAmount} = useContext(ShopContext)
+const CartItems = () => {
+  const { all_product, cartItems, removeFromCart, getTotalCartAmount } =
+    useContext(ShopContext);
+  const [promocode, setPromocode] = useState("");
+
+  function handlepromo(event) {
+    const { value } = event.target;
+    setPromocode(value);
+  }
   return (
-    <div className='cartitems'>
-        <div className="cartitems-format-main">
-            <p>Products</p>
-            <p>Title</p>
-            <p>Price</p>
-            <p>Quantity</p>
-            <p>Total</p>
-            <p>Remove</p>
-        </div>
-        <hr />
-        {all_product.map((e)=> {
-            if(cartItems[e.id] > 0) {
-                return <div>
-                            <div className="cartitems-format cartitems-format-main">
-                                <img src={e.image} alt="" className='carticon-product-icon' />
-                                <p>{e.name}</p>
-                                <p>${e.new_price}</p>
-                                <button className='cartitems-quantity'>{cartItems[e.id]}</button>
-                                <p>${e.new_price * cartItems[e.id]}</p>
-                                <img className='cartitems-remove-icon' src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
-                            </div>
-                        </div>
-            }
-            return null
-        })}
-
-        <div className="cartitems-down">
-            <div className="cartitems-total">
-                <h1>Cart Totals</h1>
-                <div>
-                    <div className="cartitems-total-item">
-                        <p>Subtotal</p>
-                        <p>${getTotalCartAmount()}</p>
-                    </div>
-                    <hr />
-                    <div className="cartitems-total-item">
-                        <p>Shipping Fee</p>
-                        <p>Free</p>
-                    </div>
-                    <hr />
-                    <div className="cartitems-total-item">
-                        <h3>Total</h3>
-                        <h3>${getTotalCartAmount()}</h3>
-                    </div>
-                </div>
-                <button>PROCEED TO CHECKOUT</button>
+    <div className="cartitems">
+      <div className="cartitems-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p></p>
+      </div>
+      <hr />
+      {all_product.map((e) => {
+        if (cartItems[e.id] > 0) {
+          return (
+            <div>
+              <div className="cartitems-format cartitems-format-main">
+                <img src={e.image} alt="" className="carticon-product-icon" />
+                <p className="producuName">{e.name}</p>
+                <p>${e.new_price}</p>
+                <button className="cartitems-quantity">
+                  {cartItems[e.id]}
+                </button>
+                <p>${e.new_price * cartItems[e.id]}</p>
+                <p
+                  className="cartitems-remove-icon"
+                  onClick={() => {
+                    removeFromCart(e.id);
+                  }}
+                  alt=""
+                >
+                  -
+                </p>
+              </div>
             </div>
-            <div className="cartitems-promocode">
-                <p>If you have a promo code, Enter it here</p>
-                <div className="cartitems-promobox">
-                    <input type="text" placeholder='pormo code'/>
-                    <button>Submit</button>
-                </div>
-            </div>
-        </div>
+          );
+        }
+        return null;
+      })}
 
+      <div className="cartitems-down">
+        <div className="cartitems-total">
+          <h1>Cart Totals</h1>
+          <div>
+            <div className="cartitems-total-item">
+              <p>Subtotal</p>
+              <p>
+                $
+                {getTotalCartAmount() -
+                  (getTotalCartAmount() > 20 && promocode === "Dems" ? 20 : 0)}
+              </p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <p>Shipping Fee</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <h3>Total</h3>
+              <h3>
+                $
+                {getTotalCartAmount() -
+                  (getTotalCartAmount() > 20 && promocode === "Dems" ? 20 : 0)}
+              </h3>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartitems-promocode">
+          <p>If you have a promo code, Enter it here</p>
+          <div className="cartitems-promobox">
+            <input
+              type="text"
+              placeholder="pormo code"
+              name="promo"
+              value={promocode}
+              onChange={handlepromo}
+            />
+            <button
+              onClick={() => {
+                promocode === "Dems"
+                  ? showAlert("success", "You get $20 discount")
+                  : showAlert("error", "Incorrect promo code");
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartItems
+export default CartItems;
--- src/Pages/LoginSignup.js
+++ src/Pages/LoginSignup.js
@@ -1,31 +1,102 @@
-import React from 'react'
-import "./CSS/LoginSignup.css"
-import SetCookie from '../hooks/SetCookie'
-import RemoveCookie from '../hooks/RemoveCookie'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import "./CSS/LoginSignup.css";
+import SetCookie from "../hooks/SetCookie";
+import RemoveCookie from "../hooks/RemoveCookie";
+import { Link } from "react-router-dom";
+import { showAlert } from "../Components/Alert/Alert";
 
 export const LoginSignup = () => {
-  const ckecklogin = ()=> {
-    RemoveCookie("user")
-    SetCookie("user", "Demessie")
+  const [isnewuser, setIsnewuser] = useState(false);
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  function handleChane(event) {
+    const { name, value } = event.target;
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
   }
 
+  const ckecklogin = () => {
+    if (!correctData) {
+      showAlert("error", "Insert correct name and password");
+    } else {
+      RemoveCookie("user");
+      SetCookie("user", "Demessie");
+    }
+  };
+
+  const correctData =
+    formData.username === "Demessie" && formData.password === "Shop1234";
+
   return (
-    <div className='loginsignup'>
+    <div className="loginsignup">
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{isnewuser === true ? "Sign up" : "Log in"}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder='Your Name' />
-          <input type="email" placeholder='Email Address' />
-          <input type="password" placeholder='Password' />
+          <div>
+            <input
+              type="text"
+              placeholder="Your Name"
+              required="true"
+              onChange={handleChane}
+              name="username"
+              value={formData.username}
+            />
+            <p>Example: Demessie</p>
+          </div>
+          <div>
+            {isnewuser && (
+              <input
+                type="email"
+                placeholder="Email Address"
+                required="true"
+                onChange={handleChane}
+                name="email"
+                value={formData.email}
+              />
+            )}
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Password"
+              required="true"
+              onChange={handleChane}
+              name="password"
+              value={formData.password}
+            />
+            <p>Example: Shop1234</p>
+          </div>
         </div>
-        <Link to="/profile"><button onClick={ckecklogin}>Continue</button></Link>
-        <p className='loginsignup-login'>Already have an account? <span>Login here</span></p>
+        <Link to={correctData ? "/profile" : "/login"}>
+          <button onClick={ckecklogin}>
+            {isnewuser === true ? "Continue" : "Log in"}
+          </button>
+        </Link>
+        <p className="loginsignup-login">
+          Are you new for our shop?{" "}
+          <span
+            onClick={() => {
+              window.scroll(0, 0);
+              showAlert("error", "system error. check it tommorow");
+              setIsnewuser(false);
+            }}
+          >
+            Create Account
+          </span>
+        </p>
         <div className="lgoinsignup-agree">
-          <input type="checkbox" name='' id='' />
+          <input type="checkbox" name="" id="" checked="true" />
           <p>By continuing, I agree to terms of use and privacy policy.</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
